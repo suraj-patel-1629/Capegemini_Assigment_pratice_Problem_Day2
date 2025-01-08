9e60c908b65b720f35c1cb93526761fbb7ec0f76
suraj patel <sp9589890205@gmail.com>
2025-01-08 14:41:44 +0530

    add

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,19 @@
 public class Calculator {
-  public static int add(int a, int b) {
-    return a + b;
+public static int add(int a, int b){
+  return a+b;
+  
+}
+
+  public static void main(String[] args) {
+    
+   int a =4;
+   int b=5;
+
+   System.out.println(add(a,b));
+
+
+
+
+  
   }
 }
